0155e9166c3a9a37f664a507242a018dc0ccc8bf
Joseph Silvashy <jpsilvashy@gmail.com>
2014-02-25 18:05:53 -0800

    cleanup

--- routes/tracer.js
+++ routes/tracer.js
@@ -1,7 +1,8 @@
 // Handle post requests to /
 exports.plot = function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  res.header("Access-Control-Allow-Methods", "POST,OPTIONS");
 
   console.log(req.body);
   console.log(req.params);
