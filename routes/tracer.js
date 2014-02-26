cfbbfd2f13cb4ba8bf14ebf4133ec9356e0fe5d9
Joseph Silvashy <jpsilvashy@gmail.com>
2014-02-25 17:36:33 -0800

    adding more tracer details

--- routes/tracer.js
+++ routes/tracer.js
@@ -4,7 +4,8 @@ exports.plot = function(req, res, next) {
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
 
   console.log(req.body);
+  console.log(req.params);
 
-  // publisher.publish('events.tracer', message.message);
+  publisher.publish('events.tracer', { message: 'tracer events recieved'});
   res.send(200);
 };
